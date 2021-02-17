9b5fc886d0cf012e1b3dfff0d29a43b6e562107e
Ian Mutch <44655976+IanMutch@users.noreply.github.com>
2021-02-17 23:09:58 +0000

    Added InputSystem package, Improved Player
    Added PlayerController.cs
    Integrated InputSystem package and player events

---
+++ WorkingTitle/Assets/Source/WorkingTitle/Input/ActionEventType.cs
@@ -0,0 +1,9 @@
+namespace WorkingTitle.Input
+{
+    public enum ActionEventType
+    {
+        Started = 1,
+        Performed = 2,
+        Canceled = 3
+    }
+}
--- WorkingTitle/Assets/Source/WorkingTitle/Player.cs
+++ WorkingTitle/Assets/Source/WorkingTitle/Player.cs
@@ -1,28 +1,15 @@
-using UnityEngine;
-
 namespace WorkingTitle
 {
     public class Player : Entity
     {
-        #region Unity Messages
+        #region Functions
 
-        protected override void Update()
+        /// <summary>
+        ///     Interacts with an object
+        /// </summary>
+        public void Interact()
         {
-            this.Rotate(new Vector2()
-            {
-                x = Input.GetAxis("Mouse Y"),
-                y = Input.GetAxis("Mouse X")
-            });
-            this.Move(new Vector3()
-            {
-                x = Input.GetAxisRaw("Horizontal"),
-                y = 0,
-                z = Input.GetAxisRaw("Vertical")
-            });
 
-            this.IsSprinting = Input.GetKey(KeyCode.LeftShift);
-
-            base.Update();
         }
 
         #endregion
